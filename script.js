33a0534784ebde85bc6d93e7d8b88d143da79639
basuabhirup <basu.abhirup27@gmail.com>
2024-05-16 21:47:53 +0530

    Implement filter functionality and store selectedFilters in localstorage

--- script.js
+++ script.js
@@ -3,11 +3,72 @@ const img = document.getElementById("theImage");
 const tryAgainButton = document.querySelector(".try-again-button");
 const attribution = document.getElementById("attribution");
 const username = document.getElementById("username");
+const filterOptions = document.querySelectorAll(".option");
+const applyButton = document.querySelector("button");
+const SELECTED_FILTERS_KEY = "sawoa-selectedFilters";
+
+// Load selected filters from localStorage (if available)
+let selectedFilters = [];
+const storedFilters = localStorage.getItem(SELECTED_FILTERS_KEY);
+if (storedFilters) {
+  try {
+    selectedFilters = JSON.parse(storedFilters);
+  } catch (error) {
+    console.error("Error parsing stored filters:", error);
+  }
+} else {
+  filterOptions.forEach((option) =>
+    selectedFilters.push(option.dataset.filter)
+  );
+}
+
+// Handle filter click and load initial states
+filterOptions.forEach((option) => {
+  const name = option.textContent.split(" (")[0].trim();
+  const filter = option.dataset.filter;
+  updateFilterUI(name, filter);
+
+  option.addEventListener("click", () => {
+    if (selectedFilters.includes(filter)) {
+      // Remove filter from selected list
+      selectedFilters.splice(selectedFilters.indexOf(filter), 1);
+    } else {
+      // Add filter to selected list
+      selectedFilters.push(filter);
+    }
+    updateFilterUI(name, filter);
+  });
+});
+
+// Update UI based on selected filters
+function updateFilterUI(name, filter) {
+  const filterSpan = document.querySelector(`[data-filter="${filter}"]`);
+  tryAgainButton.value = "Apply Filter";
+  if (selectedFilters.includes(filter)) {
+    filterSpan.innerHTML = `${name} <span role="button">(x)</span>`;
+    filterSpan.style.border = "1px solid grey";
+  } else {
+    filterSpan.innerHTML = `${name} <span role="button">(+)</span>`;
+    filterSpan.style.border = "none";
+  }
+}
 
 const getNewImage = () => {
+  let NETLIFY_FUNCTION_URL = "/.netlify/functions/getNewImage"
+  window.scrollTo({
+    top: 100,
+    behavior: "smooth",
+  });
   loader.style.display = "block"; // Starts displaying loader
   tryAgainButton.disabled = true;
-  fetch("/.netlify/functions/getNewImage")
+  tryAgainButton.value = "Try another Picture"
+
+  if (selectedFilters.length > 0) {
+    const queryString = selectedFilters.join(",")
+    NETLIFY_FUNCTION_URL += `?q=${queryString}`
+  }
+
+  fetch(NETLIFY_FUNCTION_URL)
     .then((res) => res.json())
     .then((data) => {
       // console.log(data) // View API response in browser console
@@ -19,11 +80,14 @@ const getNewImage = () => {
         attribution.style.display = "block";
         tryAgainButton.disabled = false;
         loader.style.display = "none"; // Stops displaying Loader after getting succesfully changing image
-      }, 750);
+      }, 1000);
     })
     .catch((err) => {
       console.error(err);
       tryAgainButton.disabled = false;
       loader.style.display = "none";
     });
+
+    // Update localStorage with selectedFilters
+    localStorage.setItem(SELECTED_FILTERS_KEY, JSON.stringify(selectedFilters));
 };
