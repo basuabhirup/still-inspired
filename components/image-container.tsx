55ac0b4a0b022ba5a3b06358d6483e68aa222a17
basuabhirup <basu.abhirup27@gmail.com>
2024-09-06 17:10:19 +0530

    Implement a fullscreen view for images when clicked

--- components/image-container.tsx
+++ components/image-container.tsx
@@ -5,11 +5,13 @@ import { Button } from "@/components/ui/button";
 import { useAppContext } from "@/contexts/AppContext";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { Dialog, DialogContent } from "./ui/dialog";
 
 export function ImageContainer() {
   const { imageData, isLoading, selectedFilters, getNewImage } =
     useAppContext();
   const [imageLoading, setImageLoading] = useState<boolean>(true);
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
 
   useEffect(() => {
     let timeout: NodeJS.Timeout;
@@ -34,41 +36,42 @@ export function ImageContainer() {
   }, []);
 
   return (
-    <div className="relative w-full max-w-2xl">
-      {(!!isLoading || !!imageLoading) && (
-        <div className="mx-4 lg:mx-8 min-h-[65vh] bg-gray-200 animate-pulse flex items-center justify-center text-gray-400 dark:bg-gray-800 dark:text-gray-600">
-          Loading...
-        </div>
-      )}
-
-      {!isLoading && imageData && (
-        <div
-          className={cn(
-            "flex flex-col justify-center items-center w-full min-h-[65vh]",
-            imageLoading && "absolute inset-0 invisible"
-          )}
-        >
-          <Image
-            src={
-              imageData.urls.regular ||
-              "https://source.unsplash.com/random?still-life-photography,still-life,objects,food,nature"
-            }
-            alt={imageData.alt_description || "Inspirational artwork"}
-            width={imageData.width || 400}
-            height={imageData.height || 600}
-            className="w-auto max-h-[58vh] mx-auto text-center object-cover border-4 border-black dark:border-white"
-          />
-          <div className="w-full block text-sm p-4 text-center font-thin">
-            <span>{imageData.user.name + " / "}</span>
-            <a
-              href={`${imageData.user.links.html}?utm_source=Still-a-Work-of-Art&utm_medium=referral`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Unsplash
-            </a>
+    <>
+      <div className="relative w-full max-w-2xl">
+        {(!!isLoading || !!imageLoading) && (
+          <div className="mx-4 lg:mx-8 min-h-[65vh] bg-gray-200 animate-pulse flex items-center justify-center text-gray-400 dark:bg-gray-800 dark:text-gray-600">
+            Loading...
           </div>
-          {/* <Button
+        )}
+
+        {!isLoading && imageData && (
+          <div
+            className={cn(
+              "flex flex-col justify-center items-center w-full min-h-[65vh]",
+              imageLoading && "absolute inset-0 invisible"
+            )}
+          >
+            <Image
+              src={imageData.urls.regular}
+              alt={imageData.alt_description || "Inspirational artwork"}
+              width={imageData.width || 400}
+              height={imageData.height || 600}
+              className="w-auto max-h-[58vh] mx-auto text-center object-cover border-4 border-black dark:border-white hover:cursor-pointer"
+              onClick={() =>
+                !isLoading && !imageLoading && imageData && setFullscreen(true)
+              }
+            />
+            <div className="w-full block text-sm p-4 text-center font-thin">
+              <span>{imageData.user.name + " / "}</span>
+              <a
+                href={`${imageData.user.links.html}?utm_source=Still-a-Work-of-Art&utm_medium=referral`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Unsplash
+              </a>
+            </div>
+            {/* <Button
             variant="outline"
             size="icon"
             className="absolute bottom-16 left-16 border-none"
@@ -82,35 +85,49 @@ export function ImageContainer() {
           >
             <Download className="h-4 w-4" />
           </Button> */}
-        </div>
-      )}
-
-      {!isLoading && !imageLoading && !imageData && (
-        <div className="mx-4 lg:mx-8 min-h-[60vh] bg-gray-200 flex items-center text-sm justify-center text-center text-gray-400 dark:bg-gray-600">
-          Oops! Unable to fetch image. Please try again
-        </div>
-      )}
-
-      <Button
-        onClick={getNewImage}
-        className={cn(
-          "mt-4 block mx-auto mb-1 bg-black text-white dark:text-black dark:bg-white hover:bg-gray-800",
-          isLoading && "hover:cursor-wait",
-          selectedFilters.length === 0 && "hover:cursor-not-allowed"
+          </div>
         )}
-        disabled={imageLoading || selectedFilters.length === 0}
-        size="lg"
-      >
-        {imageLoading ? "Loading new Picture" : "Try another Picture"}
-      </Button>
-      <p
-        className={cn(
-          "w-full text-[0.65rem] text-center mt-3 pt-0 mb-0 py-0",
-          selectedFilters.length > 0 ? "invisible" : "visible"
+
+        {!isLoading && !imageLoading && !imageData && (
+          <div className="mx-4 lg:mx-8 min-h-[60vh] bg-gray-200 flex items-center text-sm justify-center text-center text-gray-400 dark:bg-gray-600">
+            Oops! Unable to fetch image. Please try again
+          </div>
         )}
-      >
-        Please select at least one tag to continue
-      </p>
-    </div>
+
+        <Button
+          onClick={getNewImage}
+          className={cn(
+            "mt-4 block mx-auto mb-1 bg-black text-white dark:text-black dark:bg-white hover:bg-gray-800",
+            isLoading && "hover:cursor-wait",
+            selectedFilters.length === 0 && "hover:cursor-not-allowed"
+          )}
+          disabled={imageLoading || selectedFilters.length === 0}
+          size="lg"
+        >
+          {imageLoading ? "Loading new Picture" : "Try another Picture"}
+        </Button>
+        <p
+          className={cn(
+            "w-full text-[0.65rem] text-center mt-3 pt-0 mb-0 py-0",
+            selectedFilters.length > 0 ? "invisible" : "visible"
+          )}
+        >
+          Please select at least one tag to continue
+        </p>
+      </div>
+      <Dialog open={fullscreen} onOpenChange={setFullscreen}>
+        <DialogContent className="max-w-full h-full">
+          {!!imageData && (
+            <Image
+              src={imageData.urls.regular}
+              alt={imageData.alt_description || "Inspirational artwork"}
+              layout="fill"
+              objectFit="contain"
+              onClick={() => setFullscreen(false)}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
